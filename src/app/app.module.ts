8aa5d75687dc7685facc388d5b85ae4bcaba902f
Igor Zivanovic <igor.zivanovic@nortal.com>
2022-09-14 10:50:09 +0200

    added bootstrap and toaster

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
 import { environment } from '../environments/environment';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,26 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ToastrModule.forRoot(
+      {
+        // toastComponent: ToasterComponent,
+        // preventDuplicates: true,
+        // autoDismiss: false,
+        enableHtml: true,
+        timeOut: 5000,
+        // disableTimeOut: true,
+        progressBar: true,
+        progressAnimation: 'increasing',
+        // iconClasses: {
+        //   error: 'toast-error',
+        //   info: 'toast-info',
+        //   success: 'toast-success',
+        //   warning: 'toast-warning',
+        // },
+        // newestOnTop: false,
+        positionClass: 'toast-bottom-right'
+      }
+    ),
     environment.production ? [] : AkitaNgDevtools.forRoot()
   ],
   providers: [],
